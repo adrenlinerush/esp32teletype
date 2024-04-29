a5f45981e166d9232c66a84a128b33a71a73f276
Austin Mount <austin@adrenlinerush.net>
2024-04-29 17:48:19 -0400

    add functions for setting text options, colors, and setting cursor position

--- bios.s
+++ bios.s
@@ -15,9 +15,24 @@ KBD_FLAGS = $0002
 RELEASE = %00000001
 SHIFT   = %00000010
 
+CURSOR_X = $0003
+CURSOR_Y = $0005
+FG_COLOR = $0007
+BG_COLOR = $0009
+TEXT_FLAGS = $000A
+
+BOLD   = %00000001
+DIM    = %00000010
+ITALIC = %00001000
+ULINE  = %00010000
+BLINK  = %00100000
+INVERT = %01000000
+
   .org $8000
 
 RESET:
+    LDA #$00
+    STA TEXT_FLAGS
 
     LDA #$00 ; set PTR and FLAGS
     STA KBD_FLAGS
@@ -30,6 +45,7 @@ RESET:
     LDA #$00 ; Set VIA Port A to input
     STA DDRA
 
+    JSR SET_TEXT
     JSR CLS 
     JSR PROMPT
 
@@ -55,11 +71,34 @@ KEY_PRESSED:
 KEY_ENTER:
     JSR LFCR
     JSR PROMPT
+    ; INC TEXT_FLAGS
+    ; JSR SET_TEXT
     JMP LOOP
 
 CONTINUE:  
     JSR ECHO
     JMP LOOP
+
+TEST_BIOS:
+    LDA #$34
+    STA BG_COLOR
+    LDA #$33
+    STA FG_COLOR
+    STA CURSOR_X
+    STA CURSOR_Y
+    LDX #$01
+    LDA #$32
+    STA BG_COLOR, X
+    LDA #$31
+    STA FG_COLOR, X
+    STA CURSOR_X, X
+    STA CURSOR_Y, X
+
+    JSR SET_FG_COLOR
+    JSR SET_BG_COLOR
+    JSR MV_CURSOR
+
+    RTS
 
 IRQ:
     JSR KBD_IRQ
@@ -74,7 +113,7 @@ KBD_IRQ:
     PHA
    
 
-    LDX #$03 ; try delaying so I get correct scan code
+    LDX #$03; try delaying so I get correct scan code
     JSR DELAY_REG_X_CYCLES
     
     LDX PORTA
@@ -156,6 +195,164 @@ KBD_EXIT:
 NMI:
     RTI
 
+MV_CURSOR:
+    JSR SEND_ESC_SEQ
+    LDA CURSOR_X 
+    JSR ECHO
+    LDX #$01
+    LDA CURSOR_X, X 
+    JSR ECHO
+    LDA #$3B ; ;
+    JSR ECHO
+    LDA CURSOR_Y 
+    JSR ECHO
+    LDX #$01
+    LDA CURSOR_Y, X 
+    JSR ECHO
+    LDA #$48 ; H
+    JSR ECHO
+
+    RTS
+
+
+SET_FG_COLOR:
+    JSR SEND_ESC_SEQ
+
+    LDA FG_COLOR 
+    JSR ECHO
+    LDX #$01
+    LDA FG_COLOR, X 
+    JSR ECHO
+
+    LDA #$6D
+    JSR ECHO
+  
+    RTS
+
+SET_BG_COLOR:
+    JSR SEND_ESC_SEQ
+
+    LDA BG_COLOR 
+    JSR ECHO
+    LDX #$01
+    LDA BG_COLOR, X 
+    JSR ECHO
+
+    LDA #$6D ; m
+    JSR ECHO
+  
+    RTS
+
+SET_TEXT:
+    LDA TEXT_FLAGS
+    AND #BOLD
+    BNE SET_BOLD
+   
+    LDX #$32
+    JSR UNSET_TEXT_OPTION
+        
+    JMP ST_DIM
+
+SET_BOLD:
+    LDX #$31
+    JSR SET_TEXT_OPTION
+
+
+ST_DIM:
+    LDX #$32
+    LDA TEXT_FLAGS
+    AND #DIM
+    BNE SET_DIM
+   
+    JSR UNSET_TEXT_OPTION
+        
+    JMP ST_ITALIC
+
+SET_DIM:
+    JSR SET_TEXT_OPTION
+
+ST_ITALIC:
+    LDX #$33
+    LDA TEXT_FLAGS
+    AND #ITALIC
+    BNE SET_ITALIC
+   
+    JSR UNSET_TEXT_OPTION
+        
+    JMP ST_ULINE
+
+SET_ITALIC:
+    JSR SET_TEXT_OPTION
+
+ST_ULINE:
+    LDX #$34
+    LDA TEXT_FLAGS
+    AND #ULINE
+    BNE SET_ULINE
+   
+    JSR UNSET_TEXT_OPTION
+        
+    JMP ST_BLINK
+
+SET_ULINE:
+    JSR SET_TEXT_OPTION
+
+ST_BLINK:
+    LDX #$35
+    LDA TEXT_FLAGS
+    AND #BLINK
+    BNE SET_BLINK
+   
+    JSR UNSET_TEXT_OPTION
+        
+    JMP ST_INVERT
+
+SET_BLINK:
+    JSR SET_TEXT_OPTION
+
+ST_INVERT:
+    LDX #$37
+    LDA TEXT_FLAGS
+    AND #INVERT
+    BNE SET_INVERT
+   
+    JSR UNSET_TEXT_OPTION
+        
+    JMP ST_RETURN
+
+SET_INVERT:
+    JSR SET_TEXT_OPTION
+    
+ST_RETURN:
+    RTS
+
+SET_TEXT_OPTION:
+    JSR SEND_ESC_SEQ
+    TXA
+    JSR ECHO
+    LDA #$6D ; m
+    JSR ECHO
+  
+    RTS
+
+UNSET_TEXT_OPTION:
+    JSR SEND_ESC_SEQ
+    LDA #$32 ; 2
+    JSR ECHO
+    TXA
+    JSR ECHO
+    LDA #$6D ; m
+    JSR ECHO
+
+    RTS
+
+SEND_ESC_SEQ:
+    LDA #$1B ; Escape
+    JSR ECHO
+    LDA #$5B ; [
+    JSR ECHO
+  
+
 ECHO:
     STA PORTB
     LDA #$00
@@ -171,19 +368,13 @@ LFCR:
     RTS
 
 CLS:
-    LDA #$1B ; Escape
-    JSR ECHO
-    LDA #$5B ; [
-    JSR ECHO
+    JSR SEND_ESC_SEQ
     LDA #$32 ; 2
     JSR ECHO
     LDA #$4A ; J
     JSR ECHO
 
-    LDA #$1B ; Escape
-    JSR ECHO
-    LDA #$5B ; [
-    JSR ECHO
+    JSR SEND_ESC_SEQ
     LDA #$48 ; H
     JSR ECHO
 
